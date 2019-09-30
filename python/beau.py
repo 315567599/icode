f918d3593f41b9a99fc35b97ebe3578c285b4ca8
godzeye <315567599@qq.com>
2019-09-30 15:33:14 +0800

    ...

--- python/beau.py
+++ python/beau.py
@@ -5,3 +5,31 @@
 
 def __str__(self):
       return '<LogRecord: %s, %s, %s, %s, "%s">'%(self.name, self.levelno, self.pathname, self.lineno, self.msg)
+
+
+----------------------------------------------------------
+
+_logRecordFactory = LogRecord
+
+def setLogRecordFactory(factory):
+    global _logRecordFactory
+    _logRecordFactory = factory
+
+----------------------------------------------------------
+class PercentStyle(object):
+
+    default_format = '%(message)s'
+    asctime_format = '%(asctime)s'
+    asctime_search = '%(asctime)'
+
+    def __init__(self, fmt):
+        self._fmt = fmt or self.default_format
+
+    def usesTime(self):
+        return self._fmt.find(self.asctime_search) >= 0
+
+    def format(self, record):
+        return self._fmt % record.__dict__
+
+----------------------------------------------------------
+

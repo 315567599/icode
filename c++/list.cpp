27086f942be106972bfd8f54b14949299e4f0358
godzeye <315567599@qq.com>
2020-09-21 11:36:36 +0800

    ...

---
+++ c++/list.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main(){
+    list<char> l;
+    for(char c = 'a'; c <= 'z'; c++) {
+        l.push_back(c);
+    }
+
+    while(!l.empty()){
+        cout << l.front() << ' ';
+        l.pop_front();
+    }
+    cout << endl;
+    return 0;
+}

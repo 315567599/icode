f80d357cbecd5484705fc8697b7e29645c96454b
godzeye <315567599@qq.com>
2020-09-26 23:48:03 +0800

    ...

---
+++ c++/stdint.cpp
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int32_t i1 = -1;
+    uint32_t i2 = 2;
+
+    cout << i1 << " : " << i2 << endl;
+    return 0;
+}
